5972d4a9267e43f7340859ed708bc5c7c899e6ed
石峰铭 <sfm.shi@5253w.com>
2016-07-07 16:45:46 +0800

    change orm models

--- www/orm.py
+++ www/orm.py
@@ -81,13 +81,18 @@ class Field(object):
 
 
 class StringField(Field):
+
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-        super.__init__(name, ddl, primary_key, default)
+        super().__init__(name, ddl, primary_key, default)
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
 
 
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
-        super.__init__(name, 'boolean', False, default)
+        super().__init__(name, 'boolean', False, default)
 
 
 class IntegerField(Field):
